ff8f9d77051bd040f54307f4b8fcec07da01eb4a
Kuldeep-805 <kuldeepsingh650@gmail.com>
2025-09-04 00:15:44 +0530

    aded workflow

---
+++ environments/dev/variables.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "my-rg"
+    storage_account_name = "mstorage98765"
+    container_name       = "abc987"
+    key                  = "dev.terraform.tfstate"
+
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "59bea627-7b7a-4e47-bec4-fb0b51214f26"
+
+}
